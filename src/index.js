49254f617d80e8a0d15444afb53e8b05723127c4
ZainaliSyed <zainali.syed24@gmail.com>
2021-11-16 21:34:59 +0500

    initial base code

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import React from 'react';
 import {Text, View} from 'react-native';
 
 import Config from 'react-native-config';
-import {SOME_KEY} from 'react-native-dotenv';
 
 class App extends React.Component {
   render() {
+    console.log('Config : ', Config);
     return (
       <View
         style={{
@@ -26,7 +26,7 @@ class App extends React.Component {
         </Text>
 
         <Text style={{fontSize: 20, color: '#000', fontWeight: 'bold'}}>
-          Hello from {Config.VERSION} {SOME_KEY} App
+          Hello from {Config.VERSION} App
         </Text>
       </View>
     );
